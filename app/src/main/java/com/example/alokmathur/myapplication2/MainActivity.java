153b4b37e0329f9242355b6dffcb9fa4ac78e44e
axm26 <gaxm26@gmail.com>
2017-02-22 20:45:12 -0500

    Initial commit of test android app - tts

---
+++ app/src/main/java/com/example/alokmathur/myapplication2/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.alokmathur.myapplication2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.speech.tts.TextToSpeech;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                   tts.setLanguage(Locale.FRENCH);
+                }
+            }
+        });
+
+        // Alok/Rohan 2017-02-22
+        Button myButton = (Button) findViewById(R.id.oyebutton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Oye Oye", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                String toSpeak = "Bonjour Reeshee et Rowhhun.  Comment allez vous?  What is your name? Quelle est votre nom?";
+                tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
